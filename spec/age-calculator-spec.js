98de1f20e78b9e01f86d77c13847b55b41520dc1
Malgorzata <gosia.haniszewska@gmail.com>
2018-01-05 15:15:08 -0800

    add alive method to test

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -2,9 +2,11 @@ import { AgeCalculator } from './../js/age-calculator.js';
 
 describe('AgeCalculator', function() {
   let reusableUser;
+  let reusableUser2;
 
   beforeEach(function() {
     reusableUser = new AgeCalculator("1982-31-01", "female", "poor");
+    reusableUser2 = new AgeCalculator("1901-31-01", "female", "poor");
   })
 
   it('should count the expected lifespan based on user age', function() {
@@ -12,6 +14,10 @@ describe('AgeCalculator', function() {
     expect(reusableUser.venusLifespan()).toEqual(104);
     expect(reusableUser.marsLifespan()).toEqual(34);
     expect(reusableUser.jupiterLifespan()).toEqual(5);
+  });
+
+  it('should check if the user is already over lifespan expextation', function() {
+    expect(reusableUser.alive(468,250)).toEqual("You should be dead already!");
   });
 
 });
